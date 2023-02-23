e12d087780ba7db28924da01bc827df1cebdb30b
Ajay Kushwaha <ak383834@gmail.com>
2023-02-23 11:32:24 +0530

    Create naive_for_loops_.cpp

---
+++ naive_for_loops_.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+// #include"Myutilities.h"
+using namespace std;
+#include <iostream>
+using namespace std;
+
+void generate_substrings(vector<char> c) {
+  for (int i = 0; i < c.size(); i++) {
+    for (int j = i; j < c.size(); j++) {
+    //   int len = j - i + 1;
+      for (int k = i; k <= j; k++) {
+        cout << c[k];
+      }
+      cout <<endl;
+    }
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<char> s(n);
+  for (int i = 0; i < n; i++) {
+    cin >> s[i];
+  }
+  generate_substrings(s);
+  return 0;
+}
